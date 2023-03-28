d3a1204b766db7eca19fe8840a03458ff513eb08
Aishen <aish3n@pm.me>
2023-03-28 08:24:06 +0800

    feat: autocompletion with pear-tree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,6 +16,7 @@ return require("packer").startup(function(use)
     use "ThePrimeagen/harpoon"
     use "mbbill/undotree"
     use "gpanders/editorconfig.nvim"
+    use "tmsvg/pear-tree"
 
     use {
         "nvim-lualine/lualine.nvim",
