c03e805998b75be7218211c3730f60d5eca19e69
Aivan Ross Anuyo <aish3n@pm.me>
2023-12-28 15:15:26 +0800

    chore(kizu): set background to none as default

--- after/plugin/kizu.lua
+++ after/plugin/kizu.lua
@@ -3,6 +3,4 @@ pcall(require, "kizu")
 vim.keymap.set("n", "<leader>-", "<cmd>hi Normal guifg=#c5c8c9 guibg=none<CR>")
 vim.keymap.set("n", "<leader>=", "<cmd>hi Normal guifg=#c5c8c9 guibg=#0b0f10<CR>")
 
-if os.getenv("TMUX") then
-    vim.cmd.highlight("Normal guifg=#c5c8c9 guibg=none")
-end
+vim.cmd.highlight("Normal guifg=#c5c8c9 guibg=none")
