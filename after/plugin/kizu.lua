af280423056b7437c44e0d2300e5b0050bd53349
Aivan Ross Anuyo <aish3n@pm.me>
2023-12-28 11:12:29 +0800

    feat(kizu): add a keybind that sets opacity

--- after/plugin/kizu.lua
+++ after/plugin/kizu.lua
@@ -1,5 +1,8 @@
 pcall(require, "kizu")
 
+vim.keymap.set("n", "<leader>-", "<cmd>hi Normal guifg=#c5c8c9 guibg=none<CR>")
+vim.keymap.set("n", "<leader>=", "<cmd>hi Normal guifg=#c5c8c9 guibg=#0b0f10<CR>")
+
 if os.getenv("TMUX") then
     vim.cmd.highlight("Normal guifg=#c5c8c9 guibg=none")
 end
